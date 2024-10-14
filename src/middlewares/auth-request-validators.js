62617386666fb53141bffa6e2cda665e83f64ead
sahil dhiman <194502@nith.ac.in>
2024-10-14 14:19:15 +0530

    add validation logic for incoming request

---
+++ src/middlewares/auth-request-validators.js
@@ -0,0 +1,15 @@
+const validateUserSignup = (req,res,next)=>{
+    if(!req.body.email || !req.body.password){
+        return res.status(400).json({
+            success:false,
+            data:{},
+            message:"Something went wrong",
+            err: "Email or password missing in the request"
+        });
+    }
+    next();
+}
+
+module.exports = {
+    validateUserSignup,
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,11 @@
 const userController = require('../../controllers/user-controller.js');
 
 const express = require('express');
+const { AuthRequestValidator } = require('../../middlewares/index.js');
 const router = express.Router();
 
-router.post('/signup', userController.create);
-router.post('/signin', userController.signIn);
+router.post('/signup',AuthRequestValidator.validateUserSignup, userController.create);
+router.post('/signin',AuthRequestValidator.validateUserSignup, userController.signIn);
 router.delete('/users/:id', userController.destroy);
 
 module.exports = router;
